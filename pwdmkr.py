d08706bfd57b5f86299a635819d2f34b132fc572
root <s2m4f6@gmail.com>
2019-05-16 00:24:00 +0300

    Version 4

--- pwdmkr.py
+++ pwdmkr.py
@@ -1,3 +1,6 @@
+#pwdmkr v4
+import sys
+sys.dont_write_bytecode = True
 import random
 import string
 from functools import reduce
@@ -5,12 +8,15 @@ from functools import reduce
 
 config = {
 	"len": 16,
-	"mode": "both"
+	"mode": "both",
+	"delimiter": "",
+    "delimiter_len": 0
 }
 
 choice = None
 a = None
 length = 5
+
 
 def gen_pwd(source, max):
 	a = ""
@@ -18,21 +24,18 @@ def gen_pwd(source, max):
 		a += random.choice(source)
 	return a
 
-"""
-def gen_pwd(source, max):
-	a = ""
-	for i in range(0, max): 
-		a += random.choice(source)
-	return a
-"""
 
-def get_config():
+def cmd():
+	print("PYTHON PASSWORD MAKER")
+	#get_config
 	print("Choose Lenght of your password(defualt 16): ")
 	length = raw_input()
 	if length != "":
 		config["len"] = int(length)
+	
 	print("What type of password you want to get: letters only, numbers only or both?(l/n/b) (default both)")
 	choice = raw_input()
+	
 	if choice != '':
 		config["mode"] = choice
 	config["source"] = []
@@ -41,21 +44,23 @@ def get_config():
 	if config["mode"] in ['n', 'nummbers', 'both', 'b']:
 		config["source"] += ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] 
 
+	print("Choose delimiter: ")
+	delimiter = raw_input()
 
-print("PYTHON PASSWORD MAKER")
+	if delimiter != '':
+		config["delimiter"] = delimiter
+	
+	print("Choose delimiter length: ")
+	delimiter_len = raw_input()
 
-#print("Do you want to choose lenght of your password")
-#yn = raw_input()
-#if yn in y :	
-#	if lenght == 16 :
-#		print("What kind of delimiter you want to use?")
-#		delimiter = raw_input()
+	if delimiter_len != '':
+		config["delimiter_len"] = int(delimiter_len)
 
 
-get_config()
+	#gen_pwd
+	p = gen_pwd(config["source"], config["len"])
 
-p = gen_pwd(config["source"], config["len"])
-print("Here is your password {}".format(p))
+	p2 = reduce((lambda x, y: x + config["delimiter"] + y if ((len(x) + 1) % (config["delimiter_len"] + 1) == 0)  else x + y), list(p))
+	print("Here is your password {}".format(p2))
 
-p2 = reduce((lambda x, y: x + "-" + y if ((len(x) + 1) % (5 + 1) == 0)  else x + y), list(p))
-print("Here is your password {}".format(p2))
+cmd()
