86563f1bd59aa499e805adc30bfec493c9c218f6
Maxym <s2m4f6@gmail.com>
2019-07-21 17:56:06 +0300

    Version 5

--- pwdmkr.py
+++ pwdmkr.py
@@ -1,4 +1,4 @@
-#pwdmkr v5b2
+#pwdmkr v5 by maxrt101
 import sys
 sys.dont_write_bytecode = True
 import random
@@ -6,8 +6,8 @@ import string
 import argparse
 from functools import reduce
 
-#args
-parser = argparse.ArgumentParser(description='Python password maker')
+
+parser = argparse.ArgumentParser(description='''Python password maker. Example: ./pwdmkr.py  -l 16  -m b  -d -  -dl 4 ''')
 parser.add_argument('-v', action='store_true', help='Display version and exit')
 parser.add_argument('-l', action='store', dest='length', help='Length of password', default=16)
 parser.add_argument('-m', action='store', dest='mode', help='Mode: letters only(l), numbers only(n) or both(b)', default='b')
@@ -15,10 +15,8 @@ parser.add_argument('-d', action='store', dest='delimiter', help='Delimiter', de
 parser.add_argument('-dl', action='store', dest='delimiter_len', help='Length between delimiters', default=0)
 
 args = parser.parse_args()
-#print(args.accumulate(args.integers))
 
-version = '5b2'
-
+version = '5'
 
 config = {
 	"len": 16,
@@ -28,7 +26,6 @@ config = {
     "source": []
 }
 
-#write_config
 config["len"] = int(args.length)
 config["mode"] = args.mode
 config["delimiter"] = args.delimiter
@@ -43,18 +40,15 @@ def gen_pwd(source, max):
 
 
 def main():
-	print("PYTHON PASSWORD MAKER")
-
 	if config["mode"] in ['l', 'letters', 'both', 'b']:
 		config["source"] += string.letters
 	if config["mode"] in ['n', 'nummbers', 'both', 'b']:
 		config["source"] += ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] 
 
-	#gen_pwd
 	p = gen_pwd(config["source"], config["len"])
-
 	p2 = reduce((lambda x, y: x + config["delimiter"] + y if ((len(x) + 1) % (config["delimiter_len"] + 1) == 0)  else x + y), list(p))
-	print("Here is your password {}".format(p2))
+	print(p2)
+
 
 if args.v == True:
 	print('pwdmkr v{} (c)2019 maxrt101'.format(version))
