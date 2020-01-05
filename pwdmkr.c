0f301075d1860cb7114987689bad202cc440dab4
Maxym <s2m4f6@gmail.com>
2020-01-05 21:33:36 +0300

    Version 0.22

--- pwdmkr.c
+++ pwdmkr.c
@@ -68,7 +68,7 @@ void main(int argc, char *argv[]) {
 			while ((option = getopt(argc, argv, ":hvl:m:d:D:")) != -1) {
 				switch (option) {
 					case 'h': 
-						printf("pwdmkr v0.22\nUsage: ./pwdmkr [-h] [-v] [-l LENGTH] [-m MODE]\n\nOptions:\n\t-h - Displays this help massage\n\t-v Displays version and exits\n\t-l - Length of password. Default 16\n\t-m - Mode. Can be l(letters), n(numbers) or b(both)\n\t-d - Delimiter. Default - none\n\t-D - Delimiter length (interval)\n");
+						printf("pwdmkr v0.22\nUsage: ./pwdmkr [-h] [-v] [-l LENGTH] [-m MODE]\n\nOptions:\n\t-h - Displays this help massage\n\t-v - Displays version and exits\n\t-l - Length of password. Default 16\n\t-m - Mode. Can be l(letters), n(numbers) or b(both)\n\t-d - Delimiter. Default - none\n\t-D - Delimiter length (interval)\n");
 						exit(0);
 					case 'v':
 						printf("pwdmkr v0.22 (c)2019 maxrt101\n");
